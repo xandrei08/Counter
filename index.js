36f667ea27db57a7ea47dfc83e3289a8ef3cb185
Andrei <xandreix93@gmail.com>
2021-06-29 03:13:44 +0100

    counter

---
+++ index.js
@@ -0,0 +1,27 @@
+let count = 0;
+const value = document.querySelector("#value");
+const btns = document.querySelectorAll(".btn");
+
+btns.forEach(function (ciup) {
+  ciup.addEventListener("click", function (e) {
+    const styles = e.currentTarget.classList;
+    console.log(styles);
+    if (styles.contains("decrease")) {
+      count--;
+    } else if (styles.contains("increase")) {
+      count++;
+    } else {
+      count = 0;
+    }
+    if (count > 0) {
+      value.style.color = "green";
+    }
+    if (count < 0) {
+      value.style.color = "red";
+    }
+    if (count === 0) {
+      value.style.color = "#222";
+    }
+    value.textContent = count;
+  });
+});
